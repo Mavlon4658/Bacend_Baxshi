347f008dc97232554420cf4f933dd6358de458fb
Mavlon4658 <mavlon4658@gmail.com>
2022-08-15 15:26:16 +0500

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <!--  -->
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-title>Dashboard</v-list-item-title>
+      </v-list-item>
+      <v-row justify="center">
+        <v-expansion-panels accordion>
+          <v-expansion-panel v-for="(item, i) in menues" :key="i">
+            <v-expansion-panel-header>{{item.name}}</v-expansion-panel-header>
+            <v-expansion-panel-content
+              style="padding: 0 !important; position: relative !important"
+            >
+              <v-list-item
+                style="position: relative; left: -6%; !important; width: 111% !important;"
+                v-for="(title, i) in item.admins"
+                :key="i"
+                link
+                :to="title.to"
+              >
+                <v-list-item-title class="ms-10" v-text="title.title"></v-list-item-title>
+              </v-list-item>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-row>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>baxshiinternat.uz</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main class="">
+      <Nuxt />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      drawer: false,
+      menues: [
+        { 
+          name: 'Biz haqimizda',
+          admins: [
+                    {title: "Ko'rish", to: "/"}, 
+                    {title: 'Qo\'shish', to: "/AboutAdd"},
+                  ],
+        },
+        { 
+          name: 'Yangiliklar',
+          admins: [
+                    {title: 'Management', to: "/"}, 
+                    {title: 'Settings', to: "/"}
+                  ],
+        },
+      ],
+      
+    }
+  },
+}
+</script>
---
+++ pages/AboutAdd/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="pa-5">
+    <div>
+      <h2>Biz haqimizda ma'lumot qo'shing</h2>
+      <form action="" class="py-5">
+        <div class="pt-5">
+            <div class="label pb-3">
+                San'atni chuqurroq o'rganish haqida qisqacha matn kiriting.
+            </div>
+          <v-textarea
+            solo
+            name="input-7-4"
+            label="Matn kiriting"
+            v-model="textarea"
+            :rules="Textarea"
+          ></v-textarea>
+        </div>
+      </form>
+    </div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      textarea: '',
+
+      Textarea: [
+        (v) => !!v || 'Matnni kiritish majburiy.',
+        (v) => v.length <= 200 || 'Name must be less than 10 characters',
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
